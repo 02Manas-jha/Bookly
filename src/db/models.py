798688bba45b1d8c61df4235bc1681c3075d150a
Manas <manasadutiya@gmail.com>
2025-05-10 22:26:52 +0530

    tags table added

--- src/db/models.py
+++ src/db/models.py
@@ -67,6 +67,26 @@ class BookSchema(SQLModel, table=True):
 
     def __repr__(self):
         return f"<Book {self.title}>"
+    
+class BookTag(SQLModel, table=True):
+    book_id: uuid.UUID = Field(default=None, foreign_key="books.uid", primary_key=True)
+    tag_id: uuid.UUID = Field(default=None, foreign_key="tags.uid",primary_key=True)
+
+class Tag(SQLModel, table=True):
+    __tablename__ = "tags"
+    uid: uuid.UUID = Field(
+        sa_column=Column(pg.UUID, nullable=False, primary_key=True, default=uuid.uuid4)
+    )
+    name: str = Field(sa_column=Column(pg.VARCHAR, nullable=False))
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    books: List["BookSchema"] = Relationship(
+        link_model=BookTag,
+        back_populates="tags",
+        sa_relationship_kwargs={"lazy":"selectin"},
+    )
+
+    def __repr__(self) -> str:
+        return f"<Tag {self.name}>"
 
 class Review(SQLModel, table=True):
     __tablename__ = "reviews"
