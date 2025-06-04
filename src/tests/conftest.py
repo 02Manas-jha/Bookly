5355a86a4012f94dea1e1f2fa7584dd51716ae71
Manas <manasadutiya@gmail.com>
2025-06-05 01:45:40 +0530

    testing book endpoint

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,9 +1,12 @@
 from src.db.main import get_session
 from src.auth.dependencies import AccessTokenBearer, RoleChecker, RefreshTokenBearer
+from src.db.models import BookSchema
 from src import app
 from fastapi.testclient import TestClient
+from datetime import datetime
 from unittest.mock import Mock
 import pytest
+import uuid
 
 mock_session = Mock()
 mock_user_service = Mock()
@@ -15,7 +18,7 @@ def get_mock_session():
 
 access_token_bearer = AccessTokenBearer()
 refresh_token_bearer = RefreshTokenBearer()
-role_checker = RoleChecker()
+role_checker = RoleChecker(['admin'])
 
 app.dependency_overrides[get_session] = get_mock_session
 app.dependency_overrides[role_checker] = Mock()
@@ -36,3 +39,16 @@ def fake_book_service():
 @pytest.fixture
 def test_client():
     return TestClient(app)
+
+@pytest.fixture
+def test_book():
+    return BookSchema(
+        uid=uuid.uuid4(),
+        user_uid=uuid.uuid4(),
+        title="sample title",
+        description="sample description",
+        page_count=200,
+        language="English",
+        published_date=datetime.now(),
+        updated_at=datetime.now()
+    )
